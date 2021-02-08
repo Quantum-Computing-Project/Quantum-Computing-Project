dbbff405652eae2fa47cad6fbdc01d8c5961fd38
yianliu <yianliu@outlook.com>
2021-02-08 18:48:39 +0000

    Updated script

--- basic.py
+++ basic.py
@@ -2,3 +2,18 @@ import numpy as np
 
 # Define single-qubit H gate as gh
 gh = (2**(-0.5))*np.array([[1,1],[1,-1]])
+
+# Define tensor product function for 2 matrices
+# Need to fix problem with vectors(shape does not work)
+def tp(m1, m2):
+    (a,b) = m1.shape
+    (c,d) = m2.shape
+    pm = np.zeros((a*c,b*d))
+    i = 0
+    j = 0
+    for i in range(a):
+        for j in range(b):
+            for k in range(c):
+                for l in range(d):
+                    pm[i*c+k,j*d+l] = m1[i,j]*m2[k,l]
+    print(pm)
