9fd121e18127ea69e743dcb886cf22146e0d6e5b
yianliu <yianliu@outlook.com>
2021-02-15 11:39:12 +0000

    Changed comment about vectors

--- basic.py
+++ basic.py
@@ -4,7 +4,7 @@ import numpy as np
 gh = (2**(-0.5))*np.array([[1,1],[1,-1]])
 
 # Define tensor product function for 2 matrices
-# Need to fix problem with vectors(shape does not work)
+# Need to write vectors as np.array([[v1,v2,...]]) for tp to work
 def tp(m1, m2):
     (a,b) = m1.shape
     (c,d) = m2.shape
@@ -17,3 +17,7 @@ def tp(m1, m2):
                 for l in range(d):
                     pm[i*c+k,j*d+l] = m1[i,j]*m2[k,l]
     print(pm)
+
+a = np.array([[1,2]])
+b = np.array([[0,0,1]])
+tp(a,b)
