fe4302c396f7c6dd8dabffd932114cce5fd91e34
Matej <matejvedak@gmail.com>
2021-02-26 21:47:21 +0100

    Kronecker power function slight rework and added a function description.

--- basic.py
+++ basic.py
@@ -9,12 +9,12 @@ import numpy as np
 def kronecker_product(matrix1, matrix2):
     """
     This function takes in two matrices (or vectors) and returns a Kronecker product of the two.
-    :param matrix1: left operand
-    :param matrix2: right operand
-    :return: Kronecker product
+    :param matrix1: left operand, numpy complex128 array
+    :param matrix2: right operand, numpy complex128 array
+    :return: Kronecker product, numpy complex128 array
     """
     if not isinstance(matrix1, np.ndarray) or not isinstance(matrix2, np.ndarray):
-        raise TypeError('Inputed parameters are not numpy matrices!')
+        raise TypeError('Inputted parameters are not numpy matrices!')
 
     else:
         if len(matrix1.shape) == 1 and len(matrix2.shape) == 1:
@@ -23,7 +23,7 @@ def kronecker_product(matrix1, matrix2):
             (p,) = matrix2.shape
             q = 1
             # Modified algorithm
-            return np.array([matrix1[i // p] * matrix2[i % p] for _ in range(n*q) for i in range(m*p)])
+            return np.array([matrix1[i // p] * matrix2[i % p] for _ in range(n * q) for i in range(m * p)])
             # Might need to add dtype=np.complex128 to avoid losing the complex part, check later if it
             # doesn't work correctly
 
@@ -33,26 +33,27 @@ def kronecker_product(matrix1, matrix2):
             (p, q) = matrix2.shape
             # Found the algorithm online, wiki page of the kronecker product
             # It is modified to reflect the fact that we have a vector here
-            return np.array([[matrix1[i // p] * matrix2[i % p][j % q] for j in range(n*q)] for i in range(m*p)])
+            return np.array([[matrix1[i // p] * matrix2[i % p][j % q] for j in range(n * q)] for i in range(m * p)])
 
         elif len(matrix2.shape) == 1:
             (m, n) = matrix1.shape
             (p,) = matrix2.shape
             q = 1
             # Modified algorithm again
-            return np.array([[matrix1[i // p][j // q] * matrix2[i % p] for j in range(n*q)] for i in range(m*p)])
+            return np.array([[matrix1[i // p][j // q] * matrix2[i % p] for j in range(n * q)] for i in range(m * p)])
 
         else:
             (m, n) = matrix1.shape
             (p, q) = matrix2.shape
             # Original algorithm
-            return np.array([[matrix1[i // p][j // q] * matrix2[i % p][j % q] for j in range(n*q)] for i in range(m*p)])
+            return np.array(
+                [[matrix1[i // p][j // q] * matrix2[i % p][j % q] for j in range(n * q)] for i in range(m * p)])
 
 
 def kronecker_product_multi(*matrices):
     """
     Kronecker product implemented on an arbitrary number of matrices (or vectors). Make sure the matrices are
-    inputed in order in which you want to have them kronecker producted. For example A * B * C would be called as
+    inputted in order in which you want to have them kronecker producted. For example A * B * C would be called as
     kronecker_product_multi(A, B, C)
     :param matrices:
     :return: matrix
@@ -65,18 +66,22 @@ def kronecker_product_multi(*matrices):
             result = kronecker_product(result, matrix)
         return result
 
+
 def kronecker_product_power(matrix, power):
-    if power < 1:
+    """
+    A function that does the kronecker product on a matrix with itself a given amount of times.
+    :param matrix: A numpy complex128 array, matrix that is to be raised to a certain "kronecker" power.
+    :param power: Integer, specifies the "kronecker power" the matrix is to be raised to.
+    :return: Numpy complex128 array.
+    """
+    if power < 1 or not isinstance(power, int):
         raise SyntaxError('Power input invalid')
-    elif power == 1:
-        return matrix
     else:
-        i = 2
-        result = kronecker_product(matrix, matrix)
-        while i < power:
+        result = np.array([1])
+        for i in range(power):
             result = kronecker_product(result, matrix)
-            i = i + 1
         return result
+
 
 # Do tests here
 if __name__ == "__main__":
@@ -86,4 +91,4 @@ if __name__ == "__main__":
     print(kronecker_product(a, b))
     print(kronecker_product_multi(a, b, b))
     print(kronecker_product_multi(a, a, a))
-    print(kronecker_product_power(a,3))
+    print(kronecker_product_power(a, 3))
