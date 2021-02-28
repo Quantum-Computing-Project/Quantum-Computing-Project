35b55989aa030d64f66e42ba01e607531e9e3291
Nadiyahall <nadiyahall14@gmail.com>
2021-02-28 22:21:39 +0000

    Update QuantumRegister.py

--- QuantumRegister.py
+++ QuantumRegister.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Feb 28 17:50:09 2021
-
 @author: nadiyahall
 """
 import numpy as np
@@ -12,24 +10,22 @@ import basic
 class State(object):
     
     def __init__(self, qubits):
-    
+        
+        # If arg is Qubit object then state is just array with 1 qubit
         if isinstance(qubits, Q.Qubit):
-            
             self.state = np.array(qubits.vector)
-    
-        elif len(qubits) == 2:
             
+        # Else if args are two qubits then state is tensor product of the two
+        elif len(qubits) == 2:
             self.state = basic.kronecker_product(qubits[0].vector, qubits[1].vector)
             
+        # Else if there is a longer list of qubits then state is tensor product of them all
+        # This only work when you remove the * from the kronecker_delta_multi function so you can input a list
         elif len(qubits) > 2:
             vectors = []
             for qubit in qubits :      
                 vectors.append(qubit.vector)
-            
             self.state = basic.kronecker_product_multi(vectors)
-            # This only work when you remove the * from the kronecker_delta_
-            # multi function so you can input a list
-            
         else:
             raise TypeError('Inputted parameter is not qubit/list of qubits!')
              
