b36eab47d84029576238ba3b6b4203884483f1f7
Nadiyahall <nadiyahall14@gmail.com>
2021-02-28 20:36:08 +0000

    Update qubit.py

--- qubit.py
+++ qubit.py
@@ -7,19 +7,13 @@ class Qubit(object):
     
     def __init__(self, alpha, beta, theta, mag):
 
-        self.alpha = alpha
-        self.beta = beta
-        self.theta = theta
-        self.mag = mag
- 
-        self.norm_alpha = self.alpha/(math.sqrt(alpha**2 + beta**2))
-        self.norm_beta = self.beta/(math.sqrt(alpha**2 + beta**2))
+        self.norm_alpha = alpha/(math.sqrt(abs(alpha)**2 + abs(beta)**2))
+        self.norm_beta = beta/(math.sqrt(abs(alpha)**2 + abs(beta)**2))
 
         self.vector = np.array([self.norm_alpha, self.norm_beta])
-        self.vector = np.array([math.cos(self.theta), math.sin(self.theta)])
 
-        self.P_alpha = abs(self.norm_alpha**2)
-        self.P_beta = abs(self.norm_beta**2)
+        self.P_alpha = abs(self.norm_alpha)**2
+        self.P_beta = abs(self.norm_beta)**2    
            
     def measure(self):
 
